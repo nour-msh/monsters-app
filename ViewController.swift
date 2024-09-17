2bb18913396ad462dbe1ae534a6121225f142cee
mo mch <mchawrab.mhd@gmail.com>
2024-09-17 08:34:09 +0300

    Initializing Monster app project

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Monsters App
+//
+//  Created by Nour on 17/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
